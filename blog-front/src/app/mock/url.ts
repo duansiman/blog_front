6270cd44aeeed133d6a1a3ef2e31e7d37c6919e3
duansiman <epdcsiman@163.com>
2017-11-13 14:49:37 +0800

    用nginx解决跨域

--- blog-front/src/app/mock/url.ts
+++ blog-front/src/app/mock/url.ts
@@ -1,8 +1,3 @@
-
-import {environment} from "../../environments/environment";
-
-const URL_BASE = environment.url;
-
-export const URL_ARTICLE_HOME = URL_BASE + '/blog/api/json/article/list.do';
-export const URL_ARTICLE_RANKING = URL_BASE + '/blog/api/json/article/click/list.do';
-export const URL_ARTICLE_TOP = URL_BASE + '/blog/api/json/article/top/list.do';
+export const URL_ARTICLE_HOME = '/blog/api/json/article/list.do';
+export const URL_ARTICLE_RANKING = '/blog/api/json/article/click/list.do';
+export const URL_ARTICLE_TOP = '/blog/api/json/article/top/list.do';
